69fcec207ea7add6ffb76cfe62193e172a9fc5d9
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2021-10-19 10:25:28 +0200

    doublevadd_publisher: add launchfile for profiling
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

---
+++ doublevadd_publisher/launch/profile.launch.py
@@ -0,0 +1,60 @@
+#       ____  ____
+#      /   /\/   /
+#     /___/  \  /   Copyright (c) 2021, Xilinx®.
+#     \   \   \/    Author: Víctor Mayoral Vilches <victorma@xilinx.com>
+#      \   \
+#      /   /
+#     /___/   /\
+#     \   \  /  \
+#      \___\/\___\
+#
+# Inspired by
+# https://gitlab.com/ros-tracing/tracetools_analysis/-/blob/master/tracetools_analysis/launch/profile.launch.py
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example launch file for a profiling analysis."""
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from tracetools_launch.action import Trace
+from tracetools_trace.tools.names import DEFAULT_CONTEXT
+from tracetools_trace.tools.names import DEFAULT_EVENTS_ROS
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Trace(
+                session_name="profile",
+                events_ust=[
+                    "ros2_acceleration:vadd_pre",
+                    "ros2_acceleration:vadd_post",
+                ]
+                + DEFAULT_EVENTS_ROS,
+                events_kernel=[
+                    "sched_switch",
+                ],
+                # context_names=[
+                #     "ip",
+                # ]
+                # + DEFAULT_CONTEXT,
+            ),
+            Node(
+                package="doublevadd_publisher",
+                executable="doublevadd_publisher",
+                # arguments=["do_more"],
+                output="screen",
+            ),
+        ]
+    )
