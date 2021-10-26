5f65c2804051cbf8bb40a0b1039309bc95be97a5
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2021-10-26 11:28:45 +0200

    Add kernel resulting from SLX analysis
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

---
+++ faster_doublevadd_publisher/src/vadd_slx.cpp
@@ -0,0 +1,52 @@
+/*
+      ____  ____
+     /   /\/   /
+    /___/  \  /   Copyright (c) 2021, Xilinx®.
+    \   \   \/    Author: Víctor Mayoral Vilches <victorma@xilinx.com>
+     \   \
+     /   /
+    /___/   /\
+    \   \  /  \
+     \___\/\___\
+
+Inspired by the Vector-Add example.
+See https://github.com/Xilinx/Vitis-Tutorials/blob/master/Getting_Started/Vitis
+
+*/
+
+#define DATA_SIZE 4096
+// TRIPCOUNT identifier
+const int c_size = DATA_SIZE;
+
+extern "C" {
+    void vadd(
+            const unsigned int *in1,  // Read-Only Vector 1
+            const unsigned int *in2,  // Read-Only Vector 2
+            unsigned int *out,        // Output Result
+            int size                  // Size in integer
+            )
+    {
+#pragma HLS INTERFACE m_axi port=in1 bundle=aximm1
+#pragma HLS INTERFACE m_axi port=in2 bundle=aximm2
+#pragma HLS INTERFACE m_axi port=out bundle=aximm1
+
+        for (int j = 0; j <size; ++j) {
+// Loop annotated with HLS directives by SLX
+#pragma HLS loop_tripcount min=4096 max=4096
+  // stupidly iterate over
+                                          // it to generate load
+        //#pragma HLS loop_tripcount min = c_size max = c_size
+            for (int i = 0; i<(size/16)*16; ++i) {
+// Loop annotated with HLS directives by SLX
+#pragma HLS loop_tripcount min=4096 max=4096
+
+// Loop annotated with HLS directives by SLX
+#pragma HLS pipeline
+
+            //#pragma HLS UNROLL factor=16
+            //#pragma HLS loop_tripcount min = c_size max = c_size
+              out[i] = in1[i] + in2[i];
+            }
+        }
+    }
+}
