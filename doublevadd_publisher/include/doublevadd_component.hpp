dd5250bc81f69c6bc2faac4d2bb0668a4d8511b4
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2021-10-27 14:18:28 +0200

    Add a 'component' for doublevadd_publisher
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

---
+++ doublevadd_publisher/include/doublevadd_component.hpp
@@ -0,0 +1,64 @@
+/*
+      ____  ____
+     /   /\/   /
+    /___/  \  /   Copyright (c) 2021, Xilinx®.
+    \   \   \/    Author: Víctor Mayoral Vilches <victorma@xilinx.com>
+     \   \
+     /   /
+    /___/   /\
+    \   \  /  \
+     \___\/\___\
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef DOUBLEVADD_COMPONENT_HPP_
+#define DOUBLEVADD_COMPONENT_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <chrono>
+
+#define DATA_SIZE 4096  // 2**12
+// #define DATA_SIZE 16384  // 2**14
+// #define DATA_SIZE 65536  // 2**16
+// #define DATA_SIZE 262144  // 2**18
+
+namespace composition
+{
+
+class DoubleVaddNode : public rclcpp::Node
+{
+public:
+  explicit DoubleVaddNode(const rclcpp::NodeOptions & options);
+
+protected:
+  void on_timer();
+
+private:
+  size_t count_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::chrono::milliseconds dt_;
+
+  unsigned int in1_[DATA_SIZE];
+  unsigned int in2_[DATA_SIZE];
+  unsigned int out_[DATA_SIZE];
+
+  int publish_count_ = 0;
+
+};
+
+}  // namespace composition
+
+#endif  // DOUBLEVADD_COMPONENT_HPP_
