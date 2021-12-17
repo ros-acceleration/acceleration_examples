d6143a1aeeef58ee086fddb2ca671a78a4573f4d
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2021-12-17 16:45:45 +0100

    Re-add graph examples, particularly perception_2nodes
    Git metadata was getting corrupted when changing the
    paths of graph examples. Avoid them via these last
    two commits.
    
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

---
+++ graphs/perception/perception_2nodes/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = "perception_2nodes"
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+        (
+            "share/" + package_name + "/resource/data",
+            glob("resource/data/*"),
+        ),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    author="Víctor Mayoral-Vilches",
+    author_email="v.mayoralv@gmail.com",
+    maintainer="Víctor Mayoral-Vilches",
+    maintainer_email="v.mayoralv@gmail.com",
+    keywords=["ROS"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+    ],
+    description="A simple perception computational graph composed by 2 Nodes.",
+    license="Apache License, Version 2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "image_raw_publisher = perception_2nodes.image_raw_publisher:main",
+        ],
+    },
+)
